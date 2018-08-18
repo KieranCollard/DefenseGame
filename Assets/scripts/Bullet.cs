9ae4eb440a2834ad631fc4c2f8db69e1f3c50217
Kieran Collard <kierancollard@outlook.com>
2018-08-18 15:31:16 +1000

    Intial commit. Platform will follow mouse and fire rockets. rockets time out eventaully

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed =0;
+    public float lifeTime =0;
+
+    private float currentCountDown = 0;
+    private Vector3 travelDirection;
+	// Use this for initialization
+	void Start () {
+	
+        if(this.transform.parent == null)
+        {
+            Debug.LogAssertion("The bullet did not have an associated parent object");
+            Destroy(this);
+        }
+        else
+        {
+            this.transform.rotation = this.transform.parent.rotation;
+            //ovveride unwanted axis
+            travelDirection = new Vector3(0, this.transform.parent.up.y, 0).normalized;
+            //unparent after this to prevent bullets following the guns rotation
+            this.transform.parent = null;
+            currentCountDown = lifeTime;
+            StartCoroutine(CountDown());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(currentCountDown <= 0)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        //move forward at speed until lifetime runs out.
+        this.transform.Translate(travelDirection * speed * Time.deltaTime);
+	}
+
+    public IEnumerator CountDown()
+    {
+        while (currentCountDown >=0 )
+        {
+            --currentCountDown;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
---
+++ Assets/scripts/DefenderGun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderGun : MonoBehaviour {
+
+    //reference to our bullet prefab
+    public GameObject bullet;
+
+    AudioSource audioComponent = null;
+    // Use this for initialization
+    void Start () {
+		if(bullet == null)
+        {
+            Debug.LogAssertion("The Defender gun has not been assigned a bullet prefab");
+        }
+        audioComponent = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //get direction vector from us to mouse
+        Vector3 pivotPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        float xDifference = Input.mousePosition.x - pivotPosition.x;
+        float yDifference = Input.mousePosition.y - pivotPosition.y;
+        //calculate the angle based on direction
+        float angle = Mathf.Atan2(yDifference, xDifference) * Mathf.Rad2Deg;
+        //establish rotation around z axis
+        //offset by 90 to point 'up' instead of left
+        //and clamp so that we can't point down
+        angle -= 90;
+        if (angle > 90 || angle < -180)
+        {
+            //setting to 89 instead of 90 helps us to preserve the 'up' vector how we would expect
+            angle = 89;
+        }
+        else if (angle < -90 && angle > -180)
+        {
+            angle = -89;
+        }
+
+        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+	}
+
+    private void OnMouseUp()
+    {
+        //set the bullet as our child so that it can reference our rotation via parent command
+        GameObject.Instantiate<GameObject>(bullet, this.transform.position, this.transform.rotation, this.transform);
+        if (audioComponent != null)
+        {
+            audioComponent.PlayOneShot(audioComponent.clip);
+        }
+    }
+}
