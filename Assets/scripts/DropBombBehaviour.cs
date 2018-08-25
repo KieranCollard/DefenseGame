bd42982c0cddaa6ce682d5c5343f09da8b692983
Kieran Collard <kierancollard@outlook.com>
2018-08-25 20:33:46 +1000

    Work on initial bomb type

---
+++ Assets/scripts/DropBombBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBombBehaviour : MissileBaseObject {
+
+    //pad the sides of screen slightly to stop appearing outside or partially cut off
+    public float screenPaddingSides = 0;
+    //amount 'above' screen to spawn
+    public float screenPaddingTop = 0;
+
+    // Use this for initialization
+    void Start () {
+        //confirm we have a collider
+        if (this.GetComponent<Collider>() == null)
+        {
+            Debug.LogAssertion("The DropBomb prefab did not have a collider");
+        }
+        //spawn within bounds of screen  plusor minus padding
+        float xMin = screenPaddingSides;
+        float xMax = Screen.width - screenPaddingSides;
+        float yValue = Screen.height + screenPaddingTop;
+
+        float xValue = Random.Range(xMin, xMax);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(xValue, yValue));
+        //reset z position
+        worldPosition.z = 0;
+
+        this.transform.position = worldPosition;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //just travel forward blindly
+        this.transform.Translate(-this.transform.up * speed * Time.deltaTime);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == bulletReference)
+        {
+            ShotFromSky();
+        }
+        else if(gameObject == groundReference)
+        {
+            MadeItToGround();
+        }
+    }
+}
---
+++ Assets/scripts/MissileBaseObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBaseObject : MonoBehaviour {
+
+    public float pointScore = 0;
+    public float speed = 1;
+
+    public Transform bulletReference;
+    public Transform groundReference;
+
+	// Use this for initialization
+	void Start () {
+		if(bulletReference == null)
+        {
+            Debug.LogAssertion("Missile object did not correctly have the bullet reference " + this.gameObject.name);
+
+        }
+        if(groundReference == null)
+        {
+            Debug.LogAssertion("Missile object did not have the ground reference " + this.gameObject.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShotFromSky()
+    {
+        Destroy(this.gameObject);
+        //TODO add points via event
+    }
+
+    public void MadeItToGround()
+    {
+        Destroy(this.gameObject);
+        //TODO subtract lives via event
+    }
+}
